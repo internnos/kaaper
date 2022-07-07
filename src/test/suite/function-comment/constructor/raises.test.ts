08f9e5dfd8ee45bc09f9648bca508dc8701d05d2
jedi <amajidsinar@gmail.com>
2022-07-07 20:29:20 +0100

    test(raises): #16

--- src/test/suite/function-comment/constructor/raises.test.ts
+++ src/test/suite/function-comment/constructor/raises.test.ts
@@ -52,7 +52,6 @@ suite("function-comment: constructor: raises", () => {
     const commentText = CairoParser.parseCommentLines(functionText);
     const raisesParser = new FunctionCommentRaisesParser();
     raisesParser.setStartScope(commentText![14]);
-    // console.log(commentText![15]);
 
     const line = 15;
     assert.equal("#   decimals: decimals exceed 2^8", commentText![line].trim(), `check line ${line}`);
@@ -67,30 +66,31 @@ suite("function-comment: constructor: raises", () => {
     assert.deepEqual(targetLineParsing, resultLineParsing, `failed to get resultLineParsing line ${line}`);
   })
 
-  // test("parse line 14", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../test_assets/ERC20.cairo"
-  //   );
-  //   const functionText = CairoParser.parseFunctionScope(
-  //     pathFile,
-  //     "constructor"
-  //   );
-  //   const commentText = CairoParser.parseCommentLines(functionText);
-  //   const raisesParser = new FunctionCommentRaisesParser();
-  //   raisesParser.setStartScope(commentText![6]);
+  test("parse line 16", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../test_assets/ERC20.cairo"
+    );
+    const functionText = CairoParser.parseFunctionScope(
+      pathFile,
+      "constructor"
+    );
+    const commentText = CairoParser.parseCommentLines(functionText);
+    const raisesParser = new FunctionCommentRaisesParser();
+    raisesParser.setStartScope(commentText![14]);
 
-  //   const line = 14;
-  //   assert.equal("# Raises:", commentText![line].trim(), `check line ${line}`);
-  //   assert.notEqual(commentText![line], raisesParser.startLine);
-  //   assert.equal(true, raisesParser.isEndScope(commentText![line]), `failed to get end scope line ${line}`);
-  //   raisesParser.setEndScope(commentText![line]);
+    const line = 16;
+    assert.equal("#   recipient: cannot mint to the zero address", commentText![line].trim(), `check line ${line}`);
+    assert.notEqual(commentText![line], raisesParser.startLine);
+    const isEndScope = raisesParser.isEndScope(commentText![line]);
+    assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
 
-  //   assert.equal(false, raisesParser.runningScope, `failed to get running scope line ${line}`);
-  //   const resultLineParsing = raisesParser.parseCommentLine(commentText![line]);
+    assert.equal(true, raisesParser.runningScope, `failed to get running scope line ${line}`);
+    const resultLineParsing = raisesParser.parseCommentLine(commentText![line]);
     
-  //   assert.deepEqual(null, resultLineParsing, `failed to get resultLineParsing line ${line}`);
-  // })
+    const targetLineParsing = {name: "recipient", type: "", desc: "cannot mint to the zero address"};
+    assert.deepEqual(targetLineParsing, resultLineParsing, `failed to get resultLineParsing line ${line}`);
+  })
 
 
   // test("parse whole scope", () => {

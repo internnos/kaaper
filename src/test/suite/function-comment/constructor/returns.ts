435004e3f58dd0e6246d0266b5d08dc01c3bbcff
jedi <amajidsinar@gmail.com>
2022-07-07 19:22:46 +0100

    test(returns): #13

--- src/test/suite/function-comment/constructor/returns.ts
+++ src/test/suite/function-comment/constructor/returns.ts
@@ -40,32 +40,32 @@ suite("function-comment: constructor: returns", () => {
   });
 
 
-  // test("parse line 13", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../test_assets/ERC20.cairo"
-  //   );
-  //   const functionText = CairoParser.parseFunctionScope(
-  //     pathFile,
-  //     "constructor"
-  //   );
-  //   const commentText = CairoParser.parseCommentLines(functionText);
-  //   const returnsParser = new FunctionCommentReturnsParser();
-  //   returnsParser.setStartScope(commentText![6]);
+  test("parse line 13", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../test_assets/ERC20.cairo"
+    );
+    const functionText = CairoParser.parseFunctionScope(
+      pathFile,
+      "constructor"
+    );
+    const commentText = CairoParser.parseCommentLines(functionText);
+    const returnsParser = new FunctionCommentReturnsParser();
+    returnsParser.setStartScope(commentText![6]);
 
-  //   const line = 13;
-  //   assert.equal("#   None", commentText![line].trim(), `check line ${line}`);
-  //   assert.notEqual(commentText![line], returnsParser.startLine);
-  //   const isEndScope = returnsParser.isEndScope(commentText![line]);
-  //   assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
+    const line = 13;
+    assert.equal("#   None", commentText![line].trim(), `check line ${line}`);
+    assert.notEqual(commentText![line], returnsParser.startLine);
+    const isEndScope = returnsParser.isEndScope(commentText![line]);
+    assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
 
-  //   assert.equal(true, returnsParser.runningScope, `failed to get running scope line ${line}`);
-  //   const resultLineParsing = returnsParser.parseCommentLine(commentText![line]);
+    assert.equal(true, returnsParser.runningScope, `failed to get running scope line ${line}`);
+    const resultLineParsing = returnsParser.parseCommentLine(commentText![line]);
     
-  //   const targetLineParsing = {name: "", type: "", desc: "None"};
-  //   assert.deepEqual(targetLineParsing, resultLineParsing, `failed to get resultLineParsing line ${line}`);
-    
-  // })
+    const targetLineParsing = {name: "", type: "", desc: "None"};
+    assert.deepEqual(targetLineParsing, resultLineParsing, `failed to get resultLineParsing line ${line}`);
+  })
+
 
   // test("parse line 8", () => {
   //   const pathFile = path.resolve(

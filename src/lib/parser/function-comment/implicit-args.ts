ef59d143b6bcbdd9c4d31c7a20e319f6e66443bc
jedi <amajidsinar@gmail.com>
2022-07-07 13:42:57 +0100

    feat(implicit-args): fix missing this.name

--- src/lib/parser/function-comment/implicit-args.ts
+++ src/lib/parser/function-comment/implicit-args.ts
@@ -3,10 +3,11 @@ import { BaseCommentParser } from "../interfaces/function-comment";
 export default class FunctionCommentImplicitArgsParser extends BaseCommentParser {
   constructor() {
     super();
+    this.name = "Implicit args";
   }
 
   parseCommentLine(line: string): FunctionComment | null {
-    if (this.runningScope === true) {
+    if (this.runningScope === true && this.startLine !== line) {
       const matchCommentLines = line.match(/#\s+(.+)/);
       if (matchCommentLines) {
         const matchInterface = line.match(/#\s+(\w+)(\(?([\w\*]+)\))?$/);

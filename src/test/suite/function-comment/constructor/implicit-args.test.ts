ea9808cfcf7e01380ec8ba2b135f0f56adb5ab26
jedi <amajidsinar@gmail.com>
2022-07-07 14:24:06 +0100

    test(implicit-args): #5

--- src/test/suite/function-comment/constructor/implicit-args.test.ts
+++ src/test/suite/function-comment/constructor/implicit-args.test.ts
@@ -92,4 +92,31 @@ suite("function-comment: constructor: implicit-args", () => {
 
     assert.equal(false, implicitArgsParser.isEndScope(commentText![line]), `failed to get end scope line ${line}`);
   })
+
+  test("parse line 5", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../test_assets/ERC20.cairo"
+    );
+    const functionText = CairoParser.parseFunctionScope(
+      pathFile,
+      "constructor"
+    );
+    const commentText = CairoParser.parseCommentLines(functionText);
+    const implicitArgsParser = new FunctionCommentImplicitArgsParser();
+    implicitArgsParser.setStartScope(commentText![2]);
+
+    const line = 5;
+    assert.equal("#   range_check_ptr", commentText![line].trim(), `check line ${line}`);
+    assert.notEqual(commentText![line], implicitArgsParser.startLine);
+
+    assert.equal(true, implicitArgsParser.runningScope, `failed to get running scope line ${line}`);
+    const resultLineParsing = implicitArgsParser.parseCommentLine(commentText![line]);
+    
+    const targetLineParsing = {name: "range_check_ptr", type: "", desc: ""};
+    assert.deepEqual(targetLineParsing, resultLineParsing, `failed to get resultLineParsing line ${line}`);
+
+    assert.equal(false, implicitArgsParser.isEndScope(commentText![line]), `failed to get end scope line ${line}`);
+  })
+
 });

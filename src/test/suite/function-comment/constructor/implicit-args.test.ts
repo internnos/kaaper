65261fc4d47d763d5d250ded68a8adcecccd1c3b
jedi <amajidsinar@gmail.com>
2022-07-07 14:06:59 +0100

    test(implicit-args): #2

--- src/test/suite/function-comment/constructor/implicit-args.test.ts
+++ src/test/suite/function-comment/constructor/implicit-args.test.ts
@@ -21,6 +21,9 @@ suite("function-comment: constructor: implicit-args", () => {
     assert.equal("# Implicit args:", commentText![line].trim(), `check line ${line}`);
     implicitArgsParser.setStartScope(commentText![line]);
 
+    assert.equal(commentText![line], implicitArgsParser.startLine);
+
+
     const resultLineParsing = implicitArgsParser.parseCommentLine(commentText![line]);
     const isEndScope = implicitArgsParser.isEndScope(commentText![line]);
 
@@ -38,111 +41,17 @@ suite("function-comment: constructor: implicit-args", () => {
   });
 
 
-  // test("parse line 1", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../test_assets/ERC20.cairo"
-  //   );
-  //   const functionText = CairoParser.parseFunctionScope(
-  //     pathFile,
-  //     "constructor"
-  //   );
-  //   const commentText = CairoParser.parseCommentLines(functionText);
-  //   const implicitArgsParser = new FunctionCommentimplicitArgsParser();
-  //   implicitArgsParser.setStartScope(commentText![0]);
-
-  //   const line = 1;
-  //   assert.equal(
-  //     "#   Initialize the contract",
-  //     commentText![line].trim(),
-  //     `check line ${line}`
-  //   );
-
-  //   assert.equal("# Desc:", implicitArgsParser.startLine);
-  //   assert.notEqual(line, implicitArgsParser.startLine);
-
-  //   assert.equal(
-  //     true,
-  //     implicitArgsParser.runningScope,
-  //     `failed to get running scope line ${line}`
-  //   );
-
-  //   const isEndScope = implicitArgsParser.isEndScope(commentText![line]);
-  //   assert.equal(false, isEndScope, `failed to get end scope line ${line}`);
-
-  //   const resultLineParsing = implicitArgsParser.parseCommentLine(commentText![line]);
-
-  //   const targetLineParsing = {
-  //     name: "",
-  //     type: "",
-  //     desc: "Initialize the contract",
-  //   };
-  //   assert.deepEqual(
-  //     targetLineParsing,
-  //     resultLineParsing,
-  //     `failed to get resultLineParsing line ${line}`
-  //   );
-  // });
-
-  // test("parse line 2", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../test_assets/ERC20.cairo"
-  //   );
-  //   const functionText = CairoParser.parseFunctionScope(
-  //     pathFile,
-  //     "constructor"
-  //   );
-  //   const commentText = CairoParser.parseCommentLines(functionText);
-  //   const implicitArgsParser = new FunctionCommentimplicitArgsParser();
-  //   implicitArgsParser.setStartScope(commentText![0]);
-
-  //   const line = 2;
-  //   assert.equal(
-  //     "# Implicit args:",
-  //     commentText![line].trim(),
-  //     `check line ${line}`
-  //   );
-
-  //   assert.equal("# Desc:", implicitArgsParser.startLine);
-  //   assert.notEqual(line, implicitArgsParser.startLine);
-  //   const isEndScope = implicitArgsParser.isEndScope(commentText![line]);
-  //   assert.equal(true, isEndScope, `failed to get end scope line ${line}`);
-
-  //   implicitArgsParser.setEndScope(commentText![line]);
-
-  //   assert.equal(
-  //     false,
-  //     implicitArgsParser.runningScope,
-  //     `failed to get running scope line ${line}`
-  //   );
-
-  //   const resultLineParsing = implicitArgsParser.parseCommentLine(commentText![line]);
-
-  //   assert.deepEqual(
-  //     null,
-  //     resultLineParsing,
-  //     `failed to get resultLineParsing line ${line}`
-  //   );
-  // });
-
-  // test("parse whole comment", () => {
-  //   const pathFile = path.resolve(
-  //     __dirname,
-  //     "../../../../../test_assets/ERC20.cairo"
-  //   );
-  //   const functionText = CairoParser.parseFunctionScope(
-  //     pathFile,
-  //     "constructor"
-  //   );
-  //   const commentText = CairoParser.parseCommentLines(functionText);
-
-  //   const implicitArgsParser = new FunctionCommentimplicitArgsParser();
-
-  //   const resultLineParsing = implicitArgsParser.parseCommentLines(commentText!);
-
-  //   const targetLineParsing = [{name: "", type: "", desc: "Initialize the contract"}];
-    
-  //   assert.deepEqual(targetLineParsing, resultLineParsing, "failed to get resultLineParsing");
-  // });
+  test("parse line 1", () => {
+    const pathFile = path.resolve(
+      __dirname,
+      "../../../../../test_assets/ERC20.cairo"
+    );
+    const functionText = CairoParser.parseFunctionScope(
+      pathFile,
+      "constructor"
+    );
+    const commentText = CairoParser.parseCommentLines(functionText);
+    const implicitArgsParser = new FunctionCommentImplicitArgsParser();
+    implicitArgsParser.setStartScope(commentText![2]);
+  })
 });
